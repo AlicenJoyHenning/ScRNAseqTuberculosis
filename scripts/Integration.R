33c3e5e944ba19ad1c56782bb82b8792d7329bfd
Alicen Henning <129797527+AlicenJoyHenning@users.noreply.github.com>
2024-07-12 15:44:20 +0200

    Integration updates

--- scripts/Integration.R
+++ scripts/Integration.R
@@ -1,8 +1,9 @@
 # Library
 library(Seurat)
+library(magrittr)
 
+# 1. Read in individual objects for integration ####
 cat("Reading in objects...")
-# Read in object
 PBMC_TB_1 <- readRDS("/home/alicen/Projects/PBMC/R_objects/PBMC_TB_1.rds")
 PBMC_TB_2 <- readRDS("/home/alicen/Projects/PBMC/R_objects/PBMC_TB_2.rds")
 PBMC_TB_3 <- readRDS("/home/alicen/Projects/PBMC/R_objects/PBMC_TB_3.rds")
@@ -24,6 +25,7 @@ sample_list <- c(
 
 )
 
+# 2. Run integration functions #####
 # Subset of genes used to integrate
 cat("Selecting integration features...")
 PBMC_features <- SelectIntegrationFeatures(sample_list, nfeatures = 2000)
@@ -32,10 +34,11 @@ PBMC_features <- SelectIntegrationFeatures(sample_list, nfeatures = 2000)
 cat("Selecting integration anchors...")
 PBMC_anchors <- FindIntegrationAnchors(
   object.list = sample_list,
-  anchor.features = cellranger_features,
+  anchor.features = PBMC_features,
   normalization.method = "LogNormalize" ,
   dims = 1:30,
   k.anchor = 20)
+
 
 cat("Integrating data...")
 # Actual integration
@@ -45,8 +48,44 @@ PBMC <- IntegrateData(
   normalization.method = "LogNormalize"
 )
 
+
 cat("Saving data...")
 # Save output
 saveRDS(PBMC, "/home/alicen/Projects/PBMC/R_objects/PBMC.rds")
+
+# 3. Reduction with UMAP ####
+# Read in output if necessary
+#cat("Reading in the integrated object...")
+#PBMC <- readRDS("/home/alicen/Projects/PBMC/R_objects/PBMC.rds")
+
+cat("Reducing dimensions...")
+PBMC <- ScaleData(PBMC) %>%
+  RunPCA() %>%
+  FindNeighbors(dims = 1:30) %>%
+  FindClusters(resolution = 0.6, group.singletons = TRUE) %>%
+  RunUMAP(dims = 1:30)
+
+
+cat("Saving UMAP reduced object...")
+saveRDS(PBMC, "/home/alicen/Projects/PBMC/R_objects/PBMC_integrated.rds")
+
+
+
+# 4. Reduction with TSNE ####
+# cat("Reading in the integrated object...\n")
+# PBMC <- readRDS("/home/alicen/Projects/PBMC/R_objects/PBMC.rds")
+#
+#
+# cat("Reducing dimensions...\n")
+# PBMC <- ScaleData(PBMC) %>%
+#   RunPCA() %>%
+#   FindNeighbors(dims = 1:30) %>%
+#   FindClusters(resolution = 0.6, group.singletons = TRUE) %>%
+#   RunTSNE(dims = 1:30)
+#
+#
+# cat("Saving data...\n")
+# saveRDS(PBMC, "/home/alicen/Projects/PBMC/R_objects/PBMC_TSNE.rds")
+
 
 cat("Complete!")
